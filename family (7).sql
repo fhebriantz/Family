b2f6476774c1148acc492686ab6e387114e01442
fhebriantz <lutfi.febrianto@gmail.com>
2018-03-01 16:22:53 +0700

    Database 3 maret

--- family (7).sql
+++ family (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2018 at 12:07 PM
+-- Generation Time: Mar 01, 2018 at 10:21 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `abouts` (
   `id` int(10) UNSIGNED NOT NULL,
-  `ket` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `caption` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `desc` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,7 +43,7 @@ CREATE TABLE `abouts` (
 -- Dumping data for table `abouts`
 --
 
-INSERT INTO `abouts` (`id`, `ket`, `title`, `desc`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+INSERT INTO `abouts` (`id`, `caption`, `title`, `desc`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
 (4, 'LEADERSHIP', '\"Why we are the industry leader”', 'JAKARTA TUNGGAL CITRA’s usage of raw materials & components: metal frames, polypolymer, copolymer and others are of the finest quality materials that make us superiorly different than other manufacturers. Our Research & Development team works hard to meet the tough requirements needed to fulfill the Tricycle and Babywalker’s standard. All of our customers will certainly benefit from the high performance and value of our Tricycle and Babywalker. And we don’t stop there, no..no, we won’t rest till we got it perfect. We want to know our customer’s satisfactory and listen to their complaints. After that, it is back to the workshop and work on those complaints into compliments.', '2018-02-16 15:16:03', '', '2018-02-23 08:51:29', NULL),
 (5, 'RESULTS', '“We know our customer’s needs”', 'At www.family-trike.com, we always update the customers with our latest news and products. From the services provided after sale, we know what customers want and need, because we want them to tell us directly or contact us thru: www.family-trike.com and we meet those requirements at lightning speed.', '2018-02-16 15:16:03', '', '2018-02-23 08:53:16', NULL),
 (6, 'GLOBAL', '“We treat our customer, just like WE ARE FAMILY”', 'Our global strategy is to be the premier manufacturers of Tricycles and Babywalker’s customers and potential customers. With manufacturing facilities, sales offices in Shantou and our busy and tight schedule to constantly visit our customers, we still feel close to our distant customers, wherever they are. And we use our expert understanding of the products, after sales services and direct relationship to meet the unique needs of those customers one at a time. So, join us at the BIG FAMILY.', '2018-02-16 15:16:03', '', '2018-02-23 08:53:04', NULL);
@@ -74,7 +74,8 @@ INSERT INTO `category_products` (`id`, `category_product_name`, `created_at`, `c
 (6, 'Ride on Car ', '2018-02-16 15:17:58', '', '2018-02-16 15:17:58', NULL),
 (7, 'Battery Car ', '2018-02-16 15:17:58', '', '2018-02-16 15:17:58', NULL),
 (8, 'Baby Stroller ', '2018-02-16 15:17:58', '', '2018-02-16 15:17:58', NULL),
-(9, 'test', '2018-02-27 21:56:49', 'lutfi', '2018-02-27 21:56:49', NULL);
+(11, 'test', '2018-02-28 22:57:25', 'lutfi', '2018-02-28 22:57:25', NULL),
+(12, 'yudi', '2018-03-01 00:22:24', 'lutfi', '2018-03-01 00:22:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,8 +114,9 @@ INSERT INTO `category_subproducts` (`id`, `category_subproduct_name`, `id_catego
 (15, 'CS Series', 8, '2018-02-16 15:23:37', '', '2018-02-19 03:53:13', NULL),
 (16, 'FS Series', 8, '2018-02-16 15:23:37', '', '2018-02-19 03:53:16', NULL),
 (20, 'tambah 2', 10, '2018-02-18 21:46:32', 'lutfi', '2018-02-18 21:46:32', NULL),
-(21, 's', 12, '2018-02-21 01:51:45', 'lutfi', '2018-02-21 01:51:45', NULL),
-(24, 'test aja', 9, '2018-02-27 21:58:04', 'lutfi', '2018-02-27 21:58:04', NULL);
+(24, 'test aja', 9, '2018-02-27 21:58:04', 'lutfi', '2018-02-27 21:58:04', NULL),
+(25, 'seri 1', 12, '2018-03-01 00:22:41', 'lutfi', '2018-03-01 00:22:41', NULL),
+(26, 'sseri 1', 11, '2018-03-01 00:23:24', 'lutfi', '2018-03-01 00:23:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -281,7 +283,8 @@ INSERT INTO `product_details` (`id`, `name_product`, `id_category`, `id_category
 (6, 'Wood Bike', 5, 12, 3400000, 'Gold', 'metal', 'iron', 'gold', 'metal', 'iron', 'gold', 'metal', 'stone', 'wood', 'gold', 'wood', 'Wock', 'metal', 'content3.jpg', 'jok.jpg', 'stang.jpg', 'ban.jpg', 'bike.jpg', '2018-02-16 15:34:29', '', '2018-02-23 03:30:07', 'lutfi'),
 (7, 'Steel Bike', 5, 12, 8000000, 'Gold', 'metal', 'iron', 'gold', 'metal', 'iron', 'Wood', 'metal', 'stone', 'wood', 'gold', 'wood', 'rock', 'metal', 'content1.jpg', 'jok.jpg', 'stang.jpg', 'ban.jpg', 'bike.jpg', '2018-02-16 15:34:36', '', '2018-02-23 03:30:16', 'lutfi'),
 (8, 'Platinum Bike', 5, 12, 6000000, 'Gold', 'metal', 'iron', 'gold', 'metal', 'iron', 'gold', 'metal', 'stone', 'wood', 'gold', 'wood', 'rock', 'metal', 'content1.jpg', 'bg3.jpg', 'stang.jpg', 'ban.jpg', 'bike.jpg', '2018-02-16 15:34:29', '', '2018-02-27 00:12:11', 'lutfi'),
-(9, '3asd', 4, 7, 3, '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', 'bg1.jpg', '1.jpg', NULL, NULL, NULL, '2018-02-26 00:32:14', 'lutfi', '2018-02-27 23:26:23', 'lutfi');
+(9, '3asd', 4, 7, 3, '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', 'bg1.jpg', '1.jpg', NULL, NULL, NULL, '2018-02-26 00:32:14', 'lutfi', '2018-02-27 23:26:23', 'lutfi'),
+(10, '3asds', 3, 2, 3, '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', 'e-KTPSementara.jpg', 'e-KTPSementara.jpg', 'inta-6-smallsize.jpg', NULL, NULL, '2018-02-28 23:48:32', 'lutfi', '2018-02-28 23:55:42', 'lutfi');
 
 -- --------------------------------------------------------
 
@@ -294,17 +297,16 @@ CREATE TABLE `questions` (
   `question` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `email`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-(1, 'Berapa harga sepeda?', 'all_email@ymail.com', '2018-02-16 04:22:11', '', '2018-02-16 04:22:11', NULL);
+INSERT INTO `questions` (`id`, `question`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'Berapa harga sepeda?', 'all_email@ymail.com', '2018-02-16 04:22:11', '2018-02-16 04:22:11'),
+(2, 'Siapa nama saya? berapa harga sepeda semut? hahahaha', 'asd@gmail.com', '2018-02-28 20:33:00', '2018-02-28 20:33:00');
 
 -- --------------------------------------------------------
 
@@ -327,8 +329,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'lutfi', 'lutfi.febrianto@gmail.com', '$2y$10$ghiHFDe.fJxyvCURXrLpxOzNN7/rGD9RrNSkHbVO/wx/4g/CkD4hi', 'h0MJ3v3MkQeRK0DeKAbJ69swFappVu4YEtCQpJJdTuTzTssk8M8gmhoyikfn', '2018-02-15 00:49:31', '2018-02-18 20:26:46'),
-(2, 'asd', 'asd@gmail.com', '$2y$10$anPMIRjdy7o80fm3SlmXEOREijWJGGkHC/8WWM0HOH9/eg8CGMuhC', '8p3GXSpLeWTMMkqabgusGC6tJS7cmKvcgr6dBJq1vuoGzJ3XeDMbDk2hiA1d', '2018-02-15 10:56:31', '2018-02-18 20:24:08');
+(1, 'lutfi', 'lutfi.febrianto@gmail.com', '$2y$10$ghiHFDe.fJxyvCURXrLpxOzNN7/rGD9RrNSkHbVO/wx/4g/CkD4hi', '8w4Hp5grQzWvKFDlsQ3qw2dxM5U7h9rv714LDzBqElB6FTZ5ozbdcwiq5NXL', '2018-02-15 00:49:31', '2018-02-18 20:26:46'),
+(2, 'asd', 'asd@gmail.com', '$2y$10$ghiHFDe.fJxyvCURXrLpxOzNN7/rGD9RrNSkHbVO/wx/4g/CkD4hi', 'DJGkrhUvhU34GNFsyVmeYu0Lhc7EaXncF0iq1UavuRxjhLkqzmN7GoDknFMD', '2018-02-15 10:56:31', '2018-02-18 20:24:08');
 
 --
 -- Indexes for dumped tables
@@ -402,19 +404,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `abouts`
 --
 ALTER TABLE `abouts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `category_products`
 --
 ALTER TABLE `category_products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `category_subproducts`
 --
 ALTER TABLE `category_subproducts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `faqs`
@@ -444,13 +446,13 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
